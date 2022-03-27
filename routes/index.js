71dd666cf163be8d888fe4ad5d89a0c4ee6d00cb
Alexander Litvinenko <a.litvinenko@outlook.com>
2022-03-27 21:03:30 +0300

    Incorporate all out-of-box features of a project created by fastify-cli

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function routes(fastify, options) {
-    fastify.get("/", async (request, reply) => {
-        console.log("got ya!");
-        return { hello: "world" };
-    });
-}
-exports.default = routes;
